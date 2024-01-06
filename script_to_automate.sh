d9501c94fca428f95f7170061e64ab4743b7b42f
ishtiaqSamdani <82057297+ishtiaqSamdani@users.noreply.github.com>
2024-01-06 11:06:31 +0530

    refactor: add new comments

--- script_to_automate.sh
+++ script_to_automate.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-cd /home/ishts/Desktop/Projects/aws-data-scrap/python
+# Change to the complete path of the 'python' directory
+# Example: cd /home/ishts/Desktop/Projects/aws-data-scrap/python
+# **NOTE**: Using an absolute path is crucial when running this script with cron, as the working directory of cron jobs may differ.
+# cd python will work but not with cron
+cd <complete path>/python
+
+# Run the main Python script
 python3 -u index.py
 
-echo "completed json creation" > file.txt
+# Write a message indicating completion to file.txt
+# echo "Completed JSON creation" > file.txt
+
 for script_file in conversion-*.py; do
+    # Check if the file exists
     if [ -f "$script_file" ]; then
         echo "Running $script_file"
+        # Execute the Python script
         python3 -u "$script_file"
         echo "Finished $script_file"
         echo
